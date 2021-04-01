e644721de9f314ebd51caa1ce076fe593539265c
Pahrul Irfan <ip.5090@gmail.com>
2021-04-01 18:14:45 +0800

    pertemuan ke 4

---
+++ resources/views/identitas.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h2>identitas</h2>
+
+<a href="{{ url('/portofolio') }}">Portofilio</a>
+<a href="{{ url('/') }}">Home</a>
+
+<a href="{{ route('porto') }}">Portofilio</a>
+<a href="{{ route('home') }}">Home</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/portofolio-saya', function () {
+    return view('profile');
+})->name('porto');
+
+Route::get('/identitas-saya', 'ProfileController@index')->name('iden');
